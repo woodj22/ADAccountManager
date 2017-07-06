175a218c56e1f95a1527bcf3e690e872a29062d4
Joe Wood <joe.wood@bbc.co.uk>
2017-07-06 17:05:00 +0100

    Initiazation of connection and addition of config file

---
+++ UnlockAccount/config.example.py
@@ -0,0 +1,5 @@
+import os
+
+LDAP_USERNAME =""
+LDAP_PASSWORD =""
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='UnlockAccount',
+      version='0.1',
+      description='unlock a user account using LDAP',
+      url='',
+      author='woodj22',
+      author_email='joe.wood@bbc.co.uk',
+      license='MIT',
+      packages=['UnlockAccount'],
+      zip_safe=False)
