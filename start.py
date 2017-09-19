275ad09a560cca3011a9e2c3fd21128dd2e63097
Joe Wood <joe.wood@bbc.co.uk>
2017-09-19 14:22:10 +0100

    Add a global active directory object that can be passed using decorators

--- start.py
+++ start.py
@@ -15,6 +15,9 @@ def get_domain_server(domain):
         return Server("ldap://ldap.worldwide.core.bbc.co.uk", get_info=ALL), 'DC=worldwide,DC=core,DC=bbc,DC=co,DC=uk'
 
 
+pass_ad = click.make_pass_decorator(ActiveDirectory)
+
+
 @click.group()
 @click.option('--admin_user', help='The account name of the administrator.')
 @click.option('--admin_password', help='The password of the administrator.')
@@ -26,30 +29,29 @@ def cli(ctx, admin_user, admin_password, account_name, domain):
     if admin_user is None:
         admin_user = LDAP_ADMIN_USERNAME
         admin_password = LDAP_ADMIN_PASSWORD
-    ad = ActiveDirectory(*server, admin_user, admin_password)
-    ctx.obj['ad'] = ad
-    ctx.obj['accountName'] = account_name
+    ctx.obj = ActiveDirectory(*server, admin_user, admin_password)
+    ctx.obj.accountName = account_name
 
 
 @cli.command()
-@click.pass_context
+@pass_ad
 @click.argument('new_password')
-def change_password(ctx, new_password):
-    if ctx.obj['ad'].change_password(account_name=ctx.obj['accountName'], new_password=new_password):
+def change_password(ad, new_password):
+    if ad.change_password(account_name=ad.accountName, new_password=new_password):
         print("Your password has been changed. You just saved your company some money.")
         return
     print("your password has not been changed.")
 
 @cli.command()
-@click.pass_context
-def get_person_details(ctx):
-    print(ctx.obj['ad'].search_by_account_name(account_name=ctx.obj['accountName']))
+@pass_ad
+def get_person_details(ad):
+    print(ad.search_by_account_name(account_name=ad.accountName))
 
 
 @cli.command()
-@click.pass_context
-def unlock_account(ctx):
-    if ctx.obj['ad'].unlock_account(account_name=ctx.obj['accountName']):
+@pass_ad
+def unlock_account(ad):
+    if ad.unlock_account(account_name=ad.accountName):
         print("Your account has been unlocked.")
         return
     print("Your password has not been unlocked.")
@@ -57,3 +59,5 @@ def unlock_account(ctx):
 if __name__ == "__main__":
     cli(obj={})
 
+def main():
+    cli(obj={})
